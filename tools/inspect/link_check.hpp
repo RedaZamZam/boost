359f14784ade84ce331af91e51a7a69054d2fde3
Beman Dawes <bdawes@acm.org>
2010-11-09 18:22:33 +0000

    Release 1.45.0 beta 1
    svn path=/tags/release/Boost_1_45_0_beta1/index.htm; revision=66473

--- tools/inspect/link_check.hpp
+++ tools/inspect/link_check.hpp
@@ -19,6 +19,7 @@ namespace boost
   {
     const int m_linked_to = 1;
     const int m_present = 2;
+    const int m_nounlinked_errors = 4;
 
     class link_check : public hypertext_inspector
     {
@@ -26,17 +27,22 @@ namespace boost
       long m_unlinked_errors;
       long m_invalid_errors;
       long m_bookmark_errors;
+      long m_duplicate_bookmark_errors;
 
       typedef std::map< string, int > m_path_map;
       m_path_map m_paths; // first() is relative initial_path()
 
-      void do_url( const string & url,
-        const string & library_name, const path & full_source_path );
+      void do_url( const string & url, const string & library_name,
+        const path & full_source_path, bool no_link_errors,
+        bool allow_external_links,
+        std::string::const_iterator contents_begin, std::string::const_iterator url_start);
     public:
 
       link_check();
-      virtual const char * name() const { return "*A*"; }
-      virtual const char * desc() const { return "invalid bookmarks, invalid urls, broken links, unlinked files"; }
+      virtual const char * name() const { return "*LINK*"; }
+      virtual const char * desc() const
+      { return "invalid bookmarks, duplicate bookmarks,"
+               " invalid urls, broken links, unlinked files"; }
 
       virtual void inspect(
         const std::string & library_name,
@@ -51,10 +57,13 @@ namespace boost
 
       virtual ~link_check()
         {
-          std::cout << "  " << m_bookmark_errors << " bookmarks with invalid characters\n";
-          std::cout << "  " << m_invalid_errors << " invalid urls\n";
-          std::cout << "  " << m_broken_errors << " broken links\n";
-          std::cout << "  " << m_unlinked_errors << " unlinked files\n";
+          std::cout << "  " << m_bookmark_errors
+                    << " bookmarks with invalid characters" << line_break();
+          std::cout << "  " << m_duplicate_bookmark_errors
+                    << " duplicate bookmarks" << line_break();
+          std::cout << "  " << m_invalid_errors << " invalid urls" << line_break();
+          std::cout << "  " << m_broken_errors << " broken links" << line_break();
+          std::cout << "  " << m_unlinked_errors << " unlinked files" << line_break();
         }
     };
   }
