5fed7eb7cfaad4324e92d017a70cce4bde98483a
Beman Dawes <bdawes@acm.org>
2011-06-21 13:23:49 +0000

    Release 1.47.0 beta 1
    svn path=/tags/release/Boost_1_47_0_beta1/boost-build.jam; revision=72708

--- tools/build/v2/test/using.py
+++ tools/build/v2/test/using.py
@@ -1,43 +1,42 @@
 #!/usr/bin/python
 
-#  Copyright (C) Vladimir Prus 2005.
-#  Distributed under the Boost Software License, Version 1.0. (See
-#  accompanying file LICENSE_1_0.txt or copy at
-#  http://www.boost.org/LICENSE_1_0.txt)
+# Copyright (C) Vladimir Prus 2005.
+# Distributed under the Boost Software License, Version 1.0. (See
+# accompanying file LICENSE_1_0.txt or copy at
+# http://www.boost.org/LICENSE_1_0.txt)
 
-from BoostBuild import Tester, List
-import string
+import BoostBuild
 
-t = Tester()
+t = BoostBuild.Tester()
 
-t.write("sub/a.cpp", """ 
-int main() { return 0; }
-
+t.write("sub/a.cpp", """
+int main() {}
 """)
 
-t.write("Jamroot", """ 
+t.write("jamroot.jam", """
 using some_tool ;
 """)
 
-t.write("some_tool.jam", """ 
+t.write("some_tool.jam", """
 import project ;
 project.initialize $(__name__) ;
-
-rule init ( )
-{
-}
-
+rule init ( ) { }
 """)
 
-t.write("sub/Jamfile", """ 
-exe a : a.cpp ; 
+t.write("some_tool.py", """
+from b2.manager import get_manager
+
+get_manager().projects().initialize(__name__)
+
+def init():
+    pass
 """)
 
-
+t.write("sub/jamfile.jam", """
+exe a : a.cpp ;
+""")
 
 t.run_build_system(subdir="sub")
-
 t.expect_addition("sub/bin/$toolset/debug/a.exe")
 
 t.cleanup()
-
